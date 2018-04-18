bad55c3572abbc8397926e3016a114235454149b
Teon Sean <416152680@qq.com>
2018-04-18 12:50:35 +0800

    Framework for different implementations provided.

--- translator.py
+++ translator.py
@@ -10,57 +10,35 @@ with open('prob.dat', 'rb') as fp:
 	pCharWithPrev=pickle.load(fp)
 print('done')
 
-def probFirstChar(py0, py1, ch_idx, multiplier):
-	if not py1:
-		return pFirstChar[py0][ch_idx]*multiplier
-	alpha=0.3
-	re=alpha*pFirstChar[py0][ch_idx]+(1-alpha)*pCharWithNext[py0][py1][ch_idx]
-	return re*multiplier
+def getCandCnt(pylist):
+	cnt=1
+	for py in pylist:
+		cnt*=len(util.py2ch[py])
+	return cnt
 
-def getFirstChar(py0, py1):
-	cand=util.py2ch[py0]
-	occur=list(map(lambda x:[x, pOccur[py0][x]], cand))
-	occur.sort(key=lambda x:x[1], reverse=True)
-	multiplier=dict()
-	descend=(1-0.5)/len(cand)
-	tmp=1
-	for i in range(len(occur)):
-		multiplier[occur[i][0]]=tmp
-		tmp-=descend
-	sortable=list(map(lambda x:[x, probFirstChar(py0, py1, x, multiplier[x])], cand))
-	sortable.sort(key=lambda x:x[1], reverse=True)
-	return sortable[0][0]
+def int2array(n, pylist):
+	re=list()
+	pylist=list(reversed(pylist))
+	for py in pylist:
+		length=len(util.py2ch[py])
+		re.append(util.py2ch[py][n%length])
+		n //= length
+	return list(reversed(re))
 
-def probNextChar(py, prev, next, cur, multiplier):
-	if not next:
-		return pCharWithPrev[py][prev][cur]*multiplier
-	alpha=0.5
-	re=alpha*pCharWithPrev[py][prev][cur]+(1-alpha)*pCharWithNext[py][next][cur]
-	return re*multiplier
-
-def getNextChar(py, ch, next):
-	cand=util.py2ch[py]
-	occur=list(map(lambda x:[x, pOccur[py][x]], cand))
-	occur.sort(key=lambda x:x[1], reverse=True)
-	multiplier=dict()
-	descend=(1-0.5)/len(cand)
-	tmp=1
-	for i in range(len(occur)):
-		multiplier[occur[i][0]]=tmp
-		tmp-=descend
-	sortable=list(map(lambda x:[x, probNextChar(py, ch, next, x, multiplier[x])], cand))
-	sortable.sort(key=lambda x:x[1], reverse=True)
-	return sortable[0][0]
+def getSentenceScore(chlist, pylist):
+	return 1
 
 def translate(input):
 	pylist=util.input2array(input)
 	if len(pylist) == 0:
 		return None
-	elif len(pylist) == 1:
-		return util.idx2ch[getFirstChar(pylist[0], None)]
-	else:
-		chlist=list()
-		chlist.append(getFirstChar(pylist[0], pylist[1]))
-		for i in range(1, len(pylist)):
-			chlist.append(getNextChar(pylist[i], chlist[i-1], pylist[i+1] if i < len(pylist)-1 else None))
-		return util.array2output(chlist)
+	cnt=getCandCnt(pylist)
+	optimal_list=list()
+	optimal_score=float("-inf")
+	for i in range(cnt):
+		chlist=int2array(i, pylist)
+		score=getSentenceScore(chlist, pylist)
+		if score > optimal_score:
+			optimal_score=score
+			optimal_list=chlist
+	return util.array2output(chlist)
